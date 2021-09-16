089a1a6baf99aa44786d49b57ba02204eb0e24bf
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-09-16 10:21:46 -0700

    add lines background and responsive sign-up form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 // bring in React from React
 import React from 'react';
-import Dislikes from './components/Dislike';
-import Likes from './components/Likes';
+// import Dislikes from './components/Dislike';
+// import Likes from './components/Likes';
 import Nav from './components/Nav';
-
+import './App.css';
 
 // define our Welcome functional component
 function Welcome() {
   // what should the component return
   return (
     // Make sure to return some UI
+      <div>
+        {/* <div class="background-container">
+<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt=""/>
+<div class="stars"></div>
+<div class="twinkling"></div>
+<div class="clouds"></div>
+</div> */}
     <div>
+      <h1 id="welcome">Welcome to Memeify!</h1><br></br>
       <Nav />
-      <h1>Welcome to Memeify!</h1>
-      <Likes />
-      <Dislikes /> 
+      {/* <Likes />
+      <Dislikes />  */}
+    </div>
+    <div class="background-lines">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
     </div>
   );
 }
