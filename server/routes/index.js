7cb624e6f2bffe290c794ed1799dc847f58114bb
Jonathan Flores <jonathanflores@hotmail.com>
2021-09-15 21:24:57 -0700

    fixed routing issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // Normally we do this in server.js, but here we are doing it one layer lower so that we can be sure they are all prepended with /api
 
-router.use('/memeposts', require('./memePosts'));
+router.use('/memeposts', require('./memeposts'));
 router.use('/auth', require('./auth'));
 router.use('/users', require('./users'));
 
